9acdef56d133a12b2f5074cd712eaf671b64c676
Matias Note <matubil@gmail.com>
2023-03-06 22:12:22 -0300

    Se edita bastante el proyecto

--- js/object-exercise.js
+++ js/object-exercise.js
@@ -1,62 +1,128 @@
-const Products = [
-    {
-        name: 'PS5',
-        description: 'La consola más potente de Sony al día de la fecha',
-        price: 290000,
-        stock: true,
-        image: 'https://as.com/meristation/imagenes/2020/11/06/reportajes/1604654372_894608_1604656126_noticia_normal.jpg',
-        games: { viewValue: 'God of War Origin', value: 'gow_origin'},
-    }
-    {
-        name: 'PS4',
-        description: 'La consola anterior de Sony',
-        price: 140000,
-        stock: true,
-        image: 'https://live.mrf.io/statics/i/ps/www.muycomputer.com/wp-content/uploads/2019/07/PS4-PS5.jpg'
-    },
-    {
-        name: 'PS3',
-        description: 'La consola anterior de Sony',
-        price: 70000,
-        stock: false,
-        image: 'https://live.mrf.io/statics/i/ps/www.muycomputer.com/wp-content/uploads/2019/07/PS4-PS5.jpg',
-        joystick: true,
-        games: { viewValue: 'Uncharted 3', value: 'uncharted_3'},
-    },
-    {
-        name: 'PS2',
-        description: 'La consola anterior de Sony',
-        price: 30000,
-        stock: false,
-    },
-    {
-        name: 'XBOX Series X',
-        description: 'La consola más potente de Microsoft al día de la fecha',
-        price: 279000,
-        stock: true,
-        image: 'https://www.atajo.com.ar/images/0000000RRT-0000234357RRT-00002-Consola-Xbox-Series-X-01.jpg',
-        games: { viewValue: 'Halo Infinite', value: 'halo_infinite'},
-        joystick: true,
+// let Products = [
+//     {
+//         name: 'PS5',
+//         description: 'La consola más potente de Sony al día de la fecha',
+//         price: 290000,
+//         stock: true,
+//         image: 'https://as.com/meristation/imagenes/2020/11/06/reportajes/1604654372_894608_1604656126_noticia_normal.jpg',
+//         games: { viewValue: 'God of War Origin', value: 'gow_origin'},
+//     },
+//     {
+//         name: 'PS4',
+//         description: 'La consola anterior de Sony',
+//         price: 140000,
+//         stock: true,
+//         image: 'https://live.mrf.io/statics/i/ps/www.muycomputer.com/wp-content/uploads/2019/07/PS4-PS5.jpg'
+//     },
+//     {
+//         name: 'PS3',
+//         description: 'La consola anterior de Sony',
+//         price: 70000,
+//         stock: false,
+//         image: 'https://live.mrf.io/statics/i/ps/www.muycomputer.com/wp-content/uploads/2019/07/PS4-PS5.jpg',
+//         joystick: true,
+//         games: { viewValue: 'Uncharted 3', value: 'uncharted_3'},
+//     },
+//     {
+//         name: 'PS2',
+//         description: 'La consola anterior de Sony',
+//         price: 30000,
+//         stock: false,
+//     },
+//     {
+//         name: 'XBOX Series X',
+//         description: 'La consola más potente de Microsoft al día de la fecha',
+//         price: 279000,
+//         stock: true,
+//         image: 'https://www.atajo.com.ar/images/0000000RRT-0000234357RRT-00002-Consola-Xbox-Series-X-01.jpg',
+//         games: { viewValue: 'Halo Infinite', value: 'halo_infinite'},
+//         joystick: true,
 
-    },
-    {
-        name: 'XBOX One',
-        description: 'La consola anterior de Microsoft',
-        price: 115000,
-        stock: false,
-        image: 'https://i.blogs.es/a7dc9c/fc7174d71089999f6a7e15c1d16/1366_2000.png'
-    },
-    {
-        name: 'XBOX 360',
-        description: 'La consola de Microsoft que compite con la PS3',
-        price: 60000,
-        stock: true,
-        image: 'https://http2.mlstatic.com/D_NQ_NP_686099-MLA32731207921_112019-O.webp',
-    }
-]; 
+//     },
+//     {
+//         name: 'XBOX One',
+//         description: 'La consola anterior de Microsoft',
+//         price: 115000,
+//         stock: false,
+//         image: 'https://i.blogs.es/a7dc9c/fc7174d71089999f6a7e15c1d16/1366_2000.png'
+//     },
+//     {
+//         name: 'XBOX 360',
+//         description: 'La consola de Microsoft que compite con la PS3',
+//         price: 60000,
+//         stock: true,
+//         image: 'https://http2.mlstatic.com/D_NQ_NP_686099-MLA32731207921_112019-O.webp',
+//     }
+// ]; 
+
+// EJEMPLO DE IMPRIMIR LO DEL LOCALSTORAGE
+// swal({
+//     title: localStorage.getItem("Products") //esto lo que va a hacer es un cartel mostrandome lo que se guardo en ese localStorage en la key Products
+// })
 
 // const editButtons = document.querySelectorAll('.btn-edit') //le estoy diciendo que busque todos los botones que tengan ese btn-edit de clase *1
 
+//UNA FORMA DE CARGAR RAPIDO LA STORAGE PARA TENERLA INICIADA
+// localStorage.setItem('Products', JSON.stringify([
+//     {
+//         name: 'PS5',
+//         description: 'La consola más potente de Sony al día de la fecha',
+//         price: 290000,
+//         stock: true,
+//         image: 'https://as.com/meristation/imagenes/2020/11/06/reportajes/1604654372_894608_1604656126_noticia_normal.jpg',
+//         games: { viewValue: 'God of War Origin', value: 'gow_origin'},
+//     },
+//     {
+//         name: 'PS4',
+//         description: 'La consola anterior de Sony',
+//         price: 140000,
+//         stock: true,
+//         image: 'https://live.mrf.io/statics/i/ps/www.muycomputer.com/wp-content/uploads/2019/07/PS4-PS5.jpg'
+//     },
+//     {
+//         name: 'PS3',
+//         description: 'La consola anterior de Sony',
+//         price: 70000,
+//         stock: false,
+//         image: 'https://live.mrf.io/statics/i/ps/www.muycomputer.com/wp-content/uploads/2019/07/PS4-PS5.jpg',
+//         joystick: true,
+//         games: { viewValue: 'Uncharted 3', value: 'uncharted_3'},
+//     },
+//     {
+//         name: 'PS2',
+//         description: 'La consola anterior de Sony',
+//         price: 30000,
+//         stock: false,
+//     },
+//     {
+//         name: 'XBOX Series X',
+//         description: 'La consola más potente de Microsoft al día de la fecha',
+//         price: 279000,
+//         stock: true,
+//         image: 'https://www.atajo.com.ar/images/0000000RRT-0000234357RRT-00002-Consola-Xbox-Series-X-01.jpg',
+//         games: { viewValue: 'Halo Infinite', value: 'halo_infinite'},
+//         joystick: true,
+
+//     },
+//     {
+//         name: 'XBOX One',
+//         description: 'La consola anterior de Microsoft',
+//         price: 115000,
+//         stock: false,
+//         image: 'https://i.blogs.es/a7dc9c/fc7174d71089999f6a7e15c1d16/1366_2000.png'
+//     },
+//     {
+//         name: 'XBOX 360',
+//         description: 'La consola de Microsoft que compite con la PS3',
+//         price: 60000,
+//         stock: true,
+//         image: 'https://http2.mlstatic.com/D_NQ_NP_686099-MLA32731207921_112019-O.webp',
+//     }
+// ])) 
+
+let Products = JSON.parse(localStorage.getItem("Products")) || []; //lo que hace es instanciarlo/ inicializarlo por si no llega haber nada en el array product
+
+let favorites = [];
 
 /*      
 ?#2
@@ -92,7 +158,13 @@ let editIndex; //?      #3
 //definir una funcion para iterar el array
 function renderizarTabla(){
     //iterar el array para acceder a cada producto
-    tableBody.innerHTML = "" //lo que hace esto o para lo que está es para que cada vez que se cargue la funcion, se borre toda la tabla
+    tableBody.innerHTML = ""; //lo que hace esto o para lo que está es para que cada vez que se cargue la funcion, se borre toda la tabla
+
+    if(Products.length === 0){
+        tableBody.innerHTML = `<tr class="disabled"><td colspan="6">NO SE ENCONTRARON PRODUCTOS</td></tr>`;
+
+        return
+    }
     Products.forEach((producto, index) =>{    
         let imageSrc = '/assets/images/no-product.png'; //se declara esto para que cuando no haya una imagen, se cargue esa imagen por defecto
         if(producto.image){ //esta es una de las formas de preguntar si tiene un valor 
@@ -133,9 +205,9 @@ function renderizarTabla(){
                                     <button class="product__action-btn btn-edit" onclick="editProduct(${index})">
                                         <i class="fa-solid fa-pencil"></i>
                                     </button>
-                                    <button class="product__action-btn btn-favorite" onclick="setFavoriteProduct(${index})">
-                                        <i class="fa-regular fa-star"></i>
-                                    </button>
+                                    <button class="product__action-btn btn-favorite ${producto.favorite ? 'active' : ''}" onclick="setFavoriteProduct(${index})">
+                                    <i class="fa-regular fa-star"></i>
+                                </button>
                                 </td>
                             </tr>`
 
@@ -187,6 +259,8 @@ function renderizarTabla(){
 
 renderizarTabla()
 
+
+
 function addProduct(evt){
     evt.preventDefault(); //esto se pone porque la pagina se vive recargando, entonces se pone eso para que no se recargue la pagina
     console.dir(evt.target); //puedo ver las cosas que trae por defecto ese evento
@@ -221,10 +295,26 @@ function addProduct(evt){
     console.log(newProduct)
 
     if(editIndex >= 0){//! #3 se le pone editIndex >= 0 ya que el primero puede ingresar con 0 y un if(0) va a dar false entonces para que de true ponemos eso 
-        Products[editIndex] = newProduct
+        Products[editIndex] = newProduct;
+        swal({
+            title: `El producto se edito correctamente`,
+            icon: 'info'
+        })
     } else{
         Products.push(newProduct)//lo que estamos haciendo es cargar nuestro producto en la lista de productos
+        swal({
+            title: `El producto se agrego correctamente`,
+            icon: 'success'
+        })
     }
+
+    //Guardarlo  en el localStorage, si yo borro la cache se borra mi localStorage
+    localStorage.setItem("Products",  JSON.stringify(Products)) //localStorage.setItem(nombreKey, dataValue) 
+    // localStorage.setItem("Products", {message: "Hola local storage", value:1000) 
+    //si ademas le cargo otro valor bajo esa key va a reemplazar el valor
+    //pero si quiero guardar un objeto, ejemplo:  localStorage.setItem("Products", {message: "Hola local storage", value:1000})  tengo que convertir lo que iria de valores en un string y eso se haria con un JSON y se hace de la siguiente con lo siguiente JSON.stringify( {message: "Hola local storage", value:1000}) y quedaria asi: localStorage.setItem("Products",  JSON.stringify( {message: "Hola local storage", value:1000}))
+    // y con JSON.parse(localStorage.getItem("Products")) lo que va a ser es descontruir el JSON y hacerlo legible
+
     editIndex = undefined //esto se lo declara para que no quede el id del que modificamos porque sino no vamos a poder editar otros
     // Products.push(newProduct)//lo que estamos haciendo es cargar nuestro producto en la lista de productos
 
@@ -241,11 +331,34 @@ function addProduct(evt){
 }
 
 function deleteProduct(id){
-    Products.splice(id, 1)//el primero es el index que es, a partir de donde empieza a robar y despues va la cantidad de elementos a borrar
+    swal({
+        title: `Borrar Producto`,
+        text: `Esta seguro que desea borrar el producto ${Products[index].name}`,
+        icon: 'warning',
+        buttons: {
+            cancel: `Cancelar`,
+            delete: `Borrar`
+        }
+    }).then(value => {
+        if(value === `Borrar`){
+            Products.splice(id, 1)//el primero es el index que es, a partir de donde empieza a robar y despues va la cantidad de elementos a borrar
     
-    //el slice(id, 6) va desde id hasta el anterior al 6, ya que toma la porcion sin incluir el nro que esta a la derecha, y si ponemos slice(6,6) ahi no va a pasar nada, ya que el 6 no va a estar incluido
+            //el slice(id, 6) va desde id hasta el anterior al 6, ya que toma la porcion sin incluir el nro que esta a la derecha, y si ponemos slice(6,6) ahi no va a pasar nada, ya que el 6 no va a estar incluido
+            
+            localStorage.setItem("Products", JSON.stringify(Products));
+        
+            swal({
+                title: `Elemento Borrado Correctamente`,
+                icon: 'error'
+            })
+        
+            renderizarTabla();    
+        }else{
+            return; //es como poner return null, incluso se podria dejar sin el else, para que no haga nada
+        }
+    })
+
     
-    renderizarTabla()    
 
 }
 
@@ -280,15 +393,68 @@ function editProduct(id){           //?     #3
 }
 
 //los id no necesariamente tienen que tener el mismo nombre, lo estoy haciendo asi porque quiero pero se podria mandar otro nombre
+
 function setFavoriteProduct(id){ //?  #5
-    
+
     //llamar a la funcion desde el boton favoritos
 
 
-    //checkear si en el array productos hay algun producto cuyo indice sea distinto al elegido con la propiedad favorite: true tenmos que setearla en falso
+    //checkear si en el array productos hay algun producto cuyo indice sea distinto al elegido con la propi
+    
 
 
+    const favCount = 0; //esto sirve para lo de varios favoritos
+    const prodFiltradosFavoritos = Products.forEach( prod =>{
+        if(prod.favorite){
+            favCount++
+        }
+    })
+
+    Products.forEach((prod, index)=> {
+        if(id === index) prod.favorite = true;
+        else prod.favorite = false;
+    })
+
+    localStorage.setItem("favorites", JSON.stringify(favorites))
+
+    renderizarTabla();
+    
     //setear el producto elegido como favorite: true
+    //!aldana hizo lo de abajo
+    // Products.forEach((producto, index) =>{ 
+    //     if(producto === ture){
+    //         producto.favorite = false
+    //         let fav = document.getElementById(`btn-fav-${index}`)
+    //         fav.classList.remove('active')
+    //     }
+    // })
+    // Product[index].favorite = true;
+
+    // let fav = document.getElementById(`btn-fav-${id}`)
+    // fav.classList.add('active')
+
+
+
+    /*--------------------------si quisiese marcar varios favoritos seria de la siguiente forma ------------------------ #7
+    if(favCount >= 3){
+        favorites.shift();        
+    }
+
+    favorites.push(index);
+    
+
+    renderizarTabla();
+
+    /*mi button quedaria asi ver min 30 maso o un poquito antes que pregunto yo esto <button class="product__action-btn btn-favorite ${producto.includes(index) ? 'active' : ''}" onclick="setFavoriteProduct(${index})">
+                                        <i class="fa-regular fa-star"></i>
+                                    </button>
+
+
+                                    forma que anda
+                                    <button class="product__action-btn btn-favorite ${producto.favorite ? 'active' : ''}" onclick="setFavoriteProduct(${index})">
+                                        <i class="fa-regular fa-star"></i>
+                                    </button>
+    */
 }
 
 /*
